75cd4152c9792acf99241ac3d60888c0ca707431
Rie <>
2019-07-23 02:26:19 +0900

    左右へ移動出来るようにスクリプト書き始め

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
+
 public class GameManager : MonoBehaviour
 {
+    //定数定義：壁方向
+    public const int WALL_FRONT = 1; //前
+    public const int WALL_RIGHT = 2; //右
+    public const int WALL_BACK = 3; //後
+    public const int WALL_LEFT = 4; //左
+
+    public GameObject panelWalls; //壁全体
+
+    private int wallNo;          //現在の向いている方向
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        wallNo = WALL_FRONT;      //スタート時点では前を向く
     }
 
     // Update is called once per frame
