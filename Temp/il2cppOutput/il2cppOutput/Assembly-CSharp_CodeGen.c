4863962fd4879b9976070261400219f75f6a6001
Rie <>
2019-07-24 03:33:43 +0900

    ビルドをiosに変更　全体を720×1280にしてみる

---
+++ Temp/il2cppOutput/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,131 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void GameManager::Start()
+extern void GameManager_Start_mD77CCDBF1DA8EC5C3AE7ED955DE4E7F54B79C88E ();
+// 0x00000002 System.Void GameManager::Update()
+extern void GameManager_Update_m07DC32583BF09EB71183725B7B95FA7B4716988A ();
+// 0x00000003 System.Void GameManager::PushButtonBox()
+extern void GameManager_PushButtonBox_m18C9C37835031C2852146D8F86AE06D5C1D80600 ();
+// 0x00000004 System.Void GameManager::PushButtonMemo()
+extern void GameManager_PushButtonMemo_m8A1C47749D403E0EB649DFEA783B577E0685D2D6 ();
+// 0x00000005 System.Void GameManager::PushButtonPig()
+extern void GameManager_PushButtonPig_mAE74AF88B3A987A9141B55F5B44AE62D1B1CACDD ();
+// 0x00000006 System.Void GameManager::PushButtonHammer()
+extern void GameManager_PushButtonHammer_mF6B3A4AC899EE36965D22C1C73B7FCE85AD3AA39 ();
+// 0x00000007 System.Void GameManager::PushButtonKey()
+extern void GameManager_PushButtonKey_m29268BEEC73CD77BDE7E35764B99DEA03BE4DC30 ();
+// 0x00000008 System.Void GameManager::PushButtonMessage()
+extern void GameManager_PushButtonMessage_mACDE17D10E98067AFE20F4F183D34E6CE149FF27 ();
+// 0x00000009 System.Void GameManager::PushButtonRight()
+extern void GameManager_PushButtonRight_m426F809C3DCF5219BF183C5E806FC395A13FC212 ();
+// 0x0000000A System.Void GameManager::PushButtonLeft()
+extern void GameManager_PushButtonLeft_mB51C21120ADB8C81AD48E88B51971D8A225149E5 ();
+// 0x0000000B System.Void GameManager::ClearButtons()
+extern void GameManager_ClearButtons_mE18DD0EE8CA23832DEB9B32BD42E782E8D093882 ();
+// 0x0000000C System.Void GameManager::DisplayMessage(System.String)
+extern void GameManager_DisplayMessage_m589217A448279BCA58BC113C50AE2E93FFBC092B ();
+// 0x0000000D System.Void GameManager::DisplayWall()
+extern void GameManager_DisplayWall_mC46E752F125B6997E9D41508630A531268449B89 ();
+// 0x0000000E System.Void GameManager::PushButtonLamp1()
+extern void GameManager_PushButtonLamp1_mAA74B7E6370DAF705924779FF6C8646CD164C3ED ();
+// 0x0000000F System.Void GameManager::PushButtonLamp2()
+extern void GameManager_PushButtonLamp2_m862D11CBCDB31522FA930E4863976179C121E361 ();
+// 0x00000010 System.Void GameManager::PushButtonLamp3()
+extern void GameManager_PushButtonLamp3_m18C832788B85B5AD3B976558BA3C21EA76602C50 ();
+// 0x00000011 System.Void GameManager::ChangButtonColor(System.Int32)
+extern void GameManager_ChangButtonColor_m1825868E510A3573074357B6508D5C6996991094 ();
+// 0x00000012 System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF7F1107D38DE91EB8A57C1C3BB1A932C50CD9693 ();
+// 0x00000013 System.Void TitleManager::Start()
+extern void TitleManager_Start_m47BC2CEFB087D585632A686FDB0AA20F00F71AC9 ();
+// 0x00000014 System.Void TitleManager::Update()
+extern void TitleManager_Update_mA566807A294A61E6EA70407E07985441C91AB8BA ();
+// 0x00000015 System.Void TitleManager::PushStartButton()
+extern void TitleManager_PushStartButton_mC7DB1336A3634A03E1728DC7E21E873C5967F312 ();
+// 0x00000016 System.Void TitleManager::.ctor()
+extern void TitleManager__ctor_m7EAEDAC1DDC5181ABA94F992609AA8FA636B7C59 ();
+static Il2CppMethodPointer s_methodPointers[22] = 
+{
+	GameManager_Start_mD77CCDBF1DA8EC5C3AE7ED955DE4E7F54B79C88E,
+	GameManager_Update_m07DC32583BF09EB71183725B7B95FA7B4716988A,
+	GameManager_PushButtonBox_m18C9C37835031C2852146D8F86AE06D5C1D80600,
+	GameManager_PushButtonMemo_m8A1C47749D403E0EB649DFEA783B577E0685D2D6,
+	GameManager_PushButtonPig_mAE74AF88B3A987A9141B55F5B44AE62D1B1CACDD,
+	GameManager_PushButtonHammer_mF6B3A4AC899EE36965D22C1C73B7FCE85AD3AA39,
+	GameManager_PushButtonKey_m29268BEEC73CD77BDE7E35764B99DEA03BE4DC30,
+	GameManager_PushButtonMessage_mACDE17D10E98067AFE20F4F183D34E6CE149FF27,
+	GameManager_PushButtonRight_m426F809C3DCF5219BF183C5E806FC395A13FC212,
+	GameManager_PushButtonLeft_mB51C21120ADB8C81AD48E88B51971D8A225149E5,
+	GameManager_ClearButtons_mE18DD0EE8CA23832DEB9B32BD42E782E8D093882,
+	GameManager_DisplayMessage_m589217A448279BCA58BC113C50AE2E93FFBC092B,
+	GameManager_DisplayWall_mC46E752F125B6997E9D41508630A531268449B89,
+	GameManager_PushButtonLamp1_mAA74B7E6370DAF705924779FF6C8646CD164C3ED,
+	GameManager_PushButtonLamp2_m862D11CBCDB31522FA930E4863976179C121E361,
+	GameManager_PushButtonLamp3_m18C832788B85B5AD3B976558BA3C21EA76602C50,
+	GameManager_ChangButtonColor_m1825868E510A3573074357B6508D5C6996991094,
+	GameManager__ctor_mF7F1107D38DE91EB8A57C1C3BB1A932C50CD9693,
+	TitleManager_Start_m47BC2CEFB087D585632A686FDB0AA20F00F71AC9,
+	TitleManager_Update_mA566807A294A61E6EA70407E07985441C91AB8BA,
+	TitleManager_PushStartButton_mC7DB1336A3634A03E1728DC7E21E873C5967F312,
+	TitleManager__ctor_m7EAEDAC1DDC5181ABA94F992609AA8FA636B7C59,
+};
+static const int32_t s_InvokerIndices[22] = 
+{
+	13,
+	13,
+	13,
+	13,
+	13,
+	13,
+	13,
+	13,
+	13,
+	13,
+	13,
+	4,
+	13,
+	13,
+	13,
+	13,
+	9,
+	13,
+	13,
+	13,
+	13,
+	13,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	22,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
